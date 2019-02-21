92f693da74abe94102006f32be920da34cb028c2
retina <retina@Joabe.local>
2019-02-21 10:59:15 -0300

    first commit

---
+++ app/src/main/java/com/example/firstsample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.firstsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText edtPassword;
+    private EditText edtUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        edtUser = findViewById(R.id.edtUser);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtUser.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Type your login!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
